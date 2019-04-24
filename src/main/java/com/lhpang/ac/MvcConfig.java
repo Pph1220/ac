66042623aaa1e6b5c1ce0b69b1fd2afe27eda40e
Linhu <LinhuPang1220@163.com>
2019-04-24 23:04:48 +0800

    2019年4月24日23:04:40

---
+++ src/main/java/com/lhpang/ac/MvcConfig.java
@@ -0,0 +1,28 @@
+package com.lhpang.ac;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 类路径: com.lhpang.ac.config
+ * 描述: //TODO
+ * @author: lhpang
+ * @date: 2019-04-24 21:53
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        registry.addViewController("/").setViewName("forward:/index.html");
+
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        super.addViewControllers(registry);
+
+    }
+
+}
