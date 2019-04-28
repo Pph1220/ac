abf1a68a99d937b56fd154f4f47911ffc30d0973
lhpang <LinhuPang220@163.com>
2019-04-28 17:59:51 +0800

    2019年4月28日17:59:40

---
+++ src/main/java/com/lhpang/ac/controller/protal/IndexController.java
@@ -0,0 +1,19 @@
+package com.lhpang.ac.controller.protal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+*   类路径: com.lhpang.ac.controller.protal.IndexController
+*   描述: //TODO 
+*   @author: lhpang
+*   @date: 2019-04-28 17:24
+*/
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
