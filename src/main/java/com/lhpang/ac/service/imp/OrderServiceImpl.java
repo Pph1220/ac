fe7143e23658b14360fb665631c44ed1c97d706c
Linhu <LinhuPang1220@163.com>
2019-05-27 12:29:29 +0800

    2019年5月27日12:29:14

--- src/main/java/com/lhpang/ac/service/imp/OrderServiceImpl.java
+++ src/main/java/com/lhpang/ac/service/imp/OrderServiceImpl.java
@@ -431,7 +431,6 @@ public class OrderServiceImpl implements OrderService {
         return ServerResponse.createBySuccess(result);
     }
 
-
     /**
      * 描 述: 核心支付方法
      * @date: 2019-04-25 15:58
@@ -453,59 +452,42 @@ public class OrderServiceImpl implements OrderService {
         AlipayTradeService tradeService =
                 new AlipayTradeServiceImpl.ClientBuilder().setAlipayPublicKey(alipayConfig.getAlipayPublicKey()).setAppid(alipayConfig.getAppid()).setCharset(alipayConfig.getCharset()).setFormat(alipayConfig.getFormat()).setGatewayUrl(alipayConfig.getOpenApiDomain()).setPrivateKey(alipayConfig.getPrivateKey()).setSignType(alipayConfig.getSignType()).build();
 
-        // (必填) 商户网站订单系统中唯一订单号，64个字符以内，只能包含字母、数字、下划线，
-        // 需保证商户系统端不能重复，建议通过数据库sequence生成，
         String outTradeNo = String.valueOf(order.getOrderNo());
 
-        // (必填) 订单标题，粗略描述用户的支付目的。如“xxx品牌xxx门店当面付扫码消费”
         String subject = new StringBuilder().append("阿C外卖扫码支付").toString();
 
-        // (必填) 订单总金额，单位为元，不能超过1亿元
-        // 如果同时传入了【打折金额】,【不可打折金额】,【订单总金额】三者,则必须满足如下条件:【订单总金额】=【打折金额】+【不可打折金额】
         String totalAmount = order.getPayment().toString();
 
-        // (可选) 订单不可打折金额，可以配合商家平台配置折扣活动，如果酒水不参与打折，则将对应金额填写至此字段
-        // 如果该值未传入,但传入了【订单总金额】,【打折金额】,则该值默认为【订单总金额】-【打折金额】
         String undiscountableAmount = "0";
 
-        // 卖家支付宝账号ID，用于支持一个签约账号下支持打款到不同的收款账号，(打款到sellerId对应的支付宝账号)
-        // 如果该字段为空，则默认为与支付宝签约的商户的PID，也就是appid对应的PID
         String sellerId = "";
 
-        // 订单描述，可以对交易或商品进行一个详细地描述，比如填写"购买商品2件共15.00元"
         String body = new StringBuilder().append("订单").append(outTradeNo).append("共").append(totalAmount).toString();
 
-        // 商户操作员编号，添加此参数可以为商户操作员做销售统计
         String operatorId = "阿C本人";
 
-        // (必填) 商户门店编号，通过门店号和商家后台可以配置精准到门店的折扣信息，详询支付宝技术支持
         String storeId = "test_store_id";
 
-        // 业务扩展参数，目前可添加由支付宝分配的系统商编号(通过setSysServiceProviderId方法)，详情请咨询支付宝技术支持
         ExtendParams extendParams = new ExtendParams();
         extendParams.setSysServiceProviderId("2088100200300400500");
 
-        // 支付超时，定义为120分钟
         String timeoutExpress = "10m";
 
-        // 商品明细列表，需填写购买商品详细信息，
         List<GoodsDetail> goodsDetailList = new ArrayList<GoodsDetail>();
 
         List<OrderItem> orderItems = orderItemMapper.selectByOrderNoUserId(orderNo, userId);
 
         for (OrderItem orderItem:orderItems){
-            // 创建一个商品信息，参数含义分别为商品id（使用国标）、名称、单价（单位为分）、数量，如果需要添加商品类别，详见GoodsDetail
             GoodsDetail goodsDetail = GoodsDetail.newInstance(orderItem.getId().toString(),orderItem.getProductName(), BigDecimalUtil.mul(orderItem.getCurrentUnitPrice().doubleValue(),Double.valueOf(100)).longValue(),orderItem.getQuantity());
             goodsDetailList.add(goodsDetail);
         }
 
-        // 创建扫码支付请求builder，设置请求参数
         AlipayTradePrecreateRequestBuilder builder = new AlipayTradePrecreateRequestBuilder()
                 .setSubject(subject).setTotalAmount(totalAmount).setOutTradeNo(outTradeNo)
                 .setUndiscountableAmount(undiscountableAmount).setSellerId(sellerId).setBody(body)
                 .setOperatorId(operatorId).setStoreId(storeId).setExtendParams(extendParams)
                 .setTimeoutExpress(timeoutExpress)
-                .setNotifyUrl(alipayConfig.getNotifyUrl())//支付宝服务器主动通知商户服务器里指定的页面http路径,根据需要设置
+                .setNotifyUrl(alipayConfig.getNotifyUrl())
                 .setGoodsDetailList(goodsDetailList);
 
         AlipayF2FPrecreateResult result = tradeService.tradePrecreate(builder);
@@ -522,28 +504,23 @@ public class OrderServiceImpl implements OrderService {
                     file.mkdirs();
                 }
 
-                // 需要修改为运行机器上的路径
                 String filePath = String.format(path + "/qr-%s.png", response.getOutTradeNo());
                 String fileName = String.format("qr-%s.png",response.getOutTradeNo());
                 log.info("filePath:" + filePath);
                 ZxingUtils.getQRCodeImge(response.getQrCode(), 256, filePath);
-
 
                 resultMap.put("Img", ImgUtil.ImageToBase64(filePath));
                 return ServerResponse.createBySuccess(resultMap);
             case FAILED:
                 log.error("支付宝预下单失败!!!");
                 return createByErrorMessage("支付宝预下单失败!!!");
-
             case UNKNOWN:
                 log.error("系统异常，预下单状态未知!!!");
                 return createByErrorMessage("系统异常，预下单状态未知!!!");
-
             default:
                 log.error("不支持的交易状态，交易返回异常!!!");
                 return createByErrorMessage("不支持的交易状态，交易返回异常!!!");
         }
-
     }
     /**
      * 描 述: 阿里回调方法
